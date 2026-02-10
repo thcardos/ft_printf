c8e647f03f25c725ce0d9a72712216937def161a
thcardos <thcardos@student.42malaga.com>
2026-02-10 17:24:50 +0100

    changed main

--- main.c
+++ main.c
@@ -9,17 +9,41 @@ int	main(void)
     void *ptr = (void *)0x7ffeefbff5a8;
     void *null_ptr = NULL;
     
-    printf("Printf Original -> ");
-    result1 = printf("(%%c) %c %c%c\n (%%s) %s %s%s \n (%%d)%d%d %d %d (%%i) %i %i%i (%%u) %u%u%u (%%x) %x %x %x (%%X) %X%X%X (%%p)%p%p %%42%% %%\n", 'A', '0', '\n', "hola", "", (char *)NULL, 42, 0, INT_MIN, 0, -42, INT_MAX, 0, 42, 0, UINT_MAX, 255, 0, UINT_MAX, 255, 0, UINT_MAX, ptr, null_ptr);
+    printf("---Printf Original---\n");
+    result1 = printf("(%%c) %c %c%c ", 'A', '0', '\n');
     printf(" (%d characters were written)\n\n", result1);
-    result1 = printf("(%%c) %c %c%c\n (%%s) %s %s%s \n", 'A', '0', '\n', "hola", "", (char *)NULL);
+    result1 = printf("(%%s) %s%s%s \n", "hola", "", (char *)NULL);
     printf(" (%d characters were written)\n\n", result1);
-    result1 = printf("(%%d)%d%d %d %d \n(%%i) %i %i%i\n", 42, 0, INT_MIN, 0, -42, INT_MAX, 0);
+    result1 = printf("(%%d) %d%d %d %d \n", 42, 0, INT_MIN, 0);
+    printf(" (%d characters were written)\n\n", result1);
+    result1 = printf("(%%i) %i %i%i\n", -42, INT_MAX, 0);
+    printf(" (%d characters were written)\n\n", result1);
+    result1 = printf("(%%u) %u%u%u \n", 42, 0, UINT_MAX);
+    printf(" (%d characters were written)\n\n", result1);
+    result1 = printf("(%%x) %x %x %x \n", 255, 0, UINT_MAX);
+    printf(" (%d characters were written)\n\n", result1);
+    result1 = printf("(%%X) %X%X%X\n", 255, 0, UINT_MAX);
+    printf(" (%d characters were written)\n\n", result1);
+    result1 = printf("(%%p) %p%p %%42%% %%\n", ptr, null_ptr);
     printf(" (%d characters were written)\n\n", result1);
 
-    ft_printf("Printf Prueba -> ");
-    result2 = ft_printf("(%%c) %c %c%c\n (%%s) %s %s%s \n (%%d)%d%d %d %d (%%i) %i %i%i (%%u) %u%u%u (%%x) %x %x %x (%%X) %X%X%X (%%p)%p%p %%42%% %%\n", 'A', '0', '\n', "hola", "", (char *)NULL, 42, 0, INT_MIN, 0, -42, INT_MAX, 0, 42, 0, UINT_MAX, 255, 0, UINT_MAX, 255, 0, UINT_MAX, ptr, null_ptr);
-	ft_printf(" (%d characters were written)\n", result2);
+    ft_printf("---Printf Prueba---\n");
+    result2 = ft_printf("(%%c) %c %c%c ", 'A', '0', '\n');
+    ft_printf(" (%d characters were written)\n\n", result2);
+    result2 = ft_printf("(%%s) %s%s%s \n", "hola", "", (char *)NULL);
+    ft_printf(" (%d characters were written)\n\n", result2);
+    result2 = ft_printf("(%%d) %d%d %d %d \n", 42, 0, INT_MIN, 0);
+    ft_printf(" (%d characters were written)\n\n", result2);
+    result2 = ft_printf("(%%i) %i %i%i\n", -42, INT_MAX, 0);
+    ft_printf(" (%d characters were written)\n\n", result2);
+    result2 = ft_printf("(%%u) %u%u%u \n", 42, 0, UINT_MAX);
+    ft_printf(" (%d characters were written)\n\n", result2);
+    result2 = ft_printf("(%%x) %x %x %x \n", 255, 0, UINT_MAX);
+    ft_printf(" (%d characters were written)\n\n", result2);
+    result2 = ft_printf("(%%X) %X%X%X\n", 255, 0, UINT_MAX);
+    ft_printf(" (%d characters were written)\n\n", result2);
+    result2 = ft_printf("(%%p) %p%p %%42%% %%\n", ptr, null_ptr);
+    ft_printf(" (%d characters were written)\n\n", result2);
 
     return (0);
 }
